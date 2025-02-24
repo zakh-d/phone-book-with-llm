6c731fe63bb7a4d88bf082868df1d056764c6622
danylozakharchenko <danylo.zakharchenko@gmail.com>
2025-02-24 20:42:18 +0100

    feat: db connection

--- backend/app/contacts/router.py
+++ backend/app/contacts/router.py
@@ -1,5 +1,9 @@
-from typing import List
-from fastapi import APIRouter
+from typing import Annotated, List
+import uuid
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db import get_db
 
 from .schemas import ContactIn, ContactOut
 
@@ -17,8 +21,12 @@ async def get_contact_details(contact_id: int) -> ContactOut:
 
 
 @router.post("/")
-async def create_contact(contact: ContactIn) -> ContactOut:
-    pass
+async def create_contact(
+    contact: ContactIn, db: Annotated[AsyncSession, Depends(get_db)]
+) -> ContactOut:
+    return ContactOut(
+        id=uuid.uuid4(), phone_number=contact.phone_number, name=contact.name
+    )
 
 
 @router.put("/{contact_id}")
---
+++ backend/app/settings.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    CONTACTS_DB: str
+    CONTACTS_DB_USER: str
+    CONTACTS_DB_PASSWORD: str
+    CONTACTS_DB_HOST: str = "localhost"
+    CONTACTS_DB_PORT: int = 5432
+
+    ENVIRONMENT: str = "dev"
+
+    @property
+    def postgres_dsn(self: "Settings") -> str:
+        return (
+            f"postgresql+asyncpg://{self.CONTACTS_DB_USER}"
+            f":{self.CONTACTS_DB_PASSWORD}@{self.CONTACTS_DB_HOST}"
+            f":{self.CONTACTS_DB_PORT}/{self.CONTACTS_DB}"
+        )
+
+
+load_dotenv()
+
+settings = Settings()
